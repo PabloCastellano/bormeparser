4e42f2b59f6a966c10ba28ae4c55aba159e28cf4
Pablo Castellano <pablo@anche.no>
2015-06-13 20:29:09 +0200

    setup.py: Removed install_requires

--- setup.py
+++ setup.py
@@ -32,6 +32,6 @@ setup(
     data_files=[('', ['LICENSE.txt'])],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['requests', 'pdfminer', 'pyPdf', 'lxml'],
+    #install_requires=['requests', 'pdfminer', 'pyPdf', 'lxml'],
     test_suite = "bormeparser.tests.test_bormeparser"
 )
