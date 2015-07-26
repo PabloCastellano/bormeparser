63dddaae9277b87804d3da96af975ee2e7ce08ad
Pablo Castellano <pablo@anche.no>
2015-07-26 04:15:43 +0200

    import dependencies before import parser1

--- bormeparser/backends/__init__.py
+++ bormeparser/backends/__init__.py
@@ -1,4 +1,11 @@
-from .parser1.parser import Parser1
+
+try:
+    from PyPDF2 import PdfFileWriter, PdfFileReader
+    import pdfminer
+    from .parser1.parser import Parser1
+except ImportError:
+    pass
+
 from .pypdf2.parser import PyPDF2Parser
 
 __all__ = ['parser1', 'pypdf2']
