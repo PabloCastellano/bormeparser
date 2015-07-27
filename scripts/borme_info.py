17686d1794cfaa587e7eabd6892c053be78f694d
Pablo Castellano <pablo@anche.no>
2015-07-27 20:02:33 +0200

    borme_info.py gives prints more info

--- scripts/borme_info.py
+++ scripts/borme_info.py
@@ -12,9 +12,22 @@ if __name__ == '__main__':
         bormeparser.borme.logger.setLevel(logging.DEBUG)
 
     borme = bormeparser.parse(sys.argv[1])
-    print('CVE: %s' % borme.cve)
-    print('Fecha: %s' % borme.date)
-    print('Num: %s' % borme.num)
-    print('Provincia: %s' % borme.provincia)
-    print('Seccion: %s' % borme.seccion)
-    print('Anuncios incluidos: %d' % len(borme.get_anuncios()))
+
+    for anuncio in borme.get_anuncios():
+        print('Anuncio %d' % anuncio.id)
+        print('-' * (8 + len(str(anuncio.id))))
+        print()
+        for acto, valor in anuncio.get_actos():
+            print('  %s' % acto)
+            print('    %s' % valor)
+        print()
+
+    print('Otros')
+    print('-----')
+    print()
+    print('  CVE: %s' % borme.cve)
+    print('  Fecha: %s' % borme.date)
+    print('  Num: %s' % borme.num)
+    print('  Provincia: %s' % borme.provincia)
+    print('  Seccion: %s' % borme.seccion)
+    print('  Anuncios incluidos: %d' % len(borme.get_anuncios()))
