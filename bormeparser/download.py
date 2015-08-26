ffc6bf8179c6dd42e488b942d77bd0f8f53a8e51
Pablo Castellano <pablo@anche.no>
2015-08-26 20:51:29 +0200

    download_pdfs() ahora admite los parametros seccion= y provincia= + refactor

--- bormeparser/download.py
+++ bormeparser/download.py
@@ -38,32 +38,18 @@ def download_xml(date, filename):
     return downloaded
 
 
-# TODO: def download_pdfs(date, path, provincia)
-def download_pdfs(date, path, seccion):
-    urls = get_url_pdfs(date, seccion)
-    files = []
-
-    for url in urls.values():
-        filename = url.split('/')[-1]
-        full_path = os.path.join(path, filename)
-        downloaded = download_url(url, full_path)
-
-        if downloaded:
-            files.append(full_path)
-            logger.debug('Downloaded %s' % filename)
-        else:
-            logger.error('Error downloading %s' % url)
-
-        #assert os.path.exists(filepdf)
-        #assert os.path.getsize(filepdf) == int(url.attrib['szBytes'])
-
+def download_pdfs(date, path, provincia=None, seccion=None):
+    """ Descarga BORMEs PDF de las provincia, la seccion y la fecha indicada """
+    urls = get_url_pdfs(date, provincia=provincia, seccion=seccion)
+    files = download_urls(urls, path)
     return True, files
 
 
 # date = (year, month, date) or datetime.date
-# seccion = ('A', 'B', 'C') or class SECCION
-# province = class PROVINCIA
+# seccion = ('A', 'B', 'C') or SECCION.A, SECCION.B, ...
+# province = class PROVINCIA: PROVINCIA.MALAGA, PROVINCIA.MADRID, ...
 def download_pdf(date, filename, seccion, provincia, parse=False):
+    """ Descarga BORME PDF de la provincia, la seccion y la fecha indicada """
     url = get_url_pdf(date, seccion, provincia)
     downloaded = download_url(url, filename)
 
@@ -112,7 +98,28 @@ def get_nbo_from_xml(xml_path):
     return tree.xpath('//sumario/diario')[0].attrib['nbo']
 
 
-def get_url_pdfs(date, seccion):
+def get_url_pdfs_provincia(date, provincia):
+    """ Obtiene las URLs para descargar los BORMEs de la provincia y fecha indicada """
+    url = get_url_xml(date)
+    tree = etree.parse(url)
+
+    if tree.getroot().tag != 'sumario':
+        raise BormeDoesntExistException
+
+    urls = {}
+    for item in tree.xpath('//sumario/diario/seccion/emisor/item'):
+        prov = item.xpath('titulo')[0].text
+        if prov != provincia:
+            continue
+        url = URL_BASE + item.xpath('urlPdf')[0].text
+        seccion = item.getparent().getparent().get('num')
+        urls[seccion] = url
+
+    return urls
+
+
+def get_url_pdfs_seccion(date, seccion):
+    """ Obtiene las URLs para descargar los BORMEs de la seccion y fecha indicada """
     url = get_url_xml(date)
     tree = etree.parse(url)
 
@@ -128,9 +135,21 @@ def get_url_pdfs(date, seccion):
     return urls
 
 
+def get_url_pdfs(date, seccion=None, provincia=None):
+    if seccion and not provincia:
+        urls = get_url_pdfs_seccion(date, seccion)
+    elif provincia and not seccion:
+        urls = get_url_pdfs_provincia(date, provincia)
+    elif provincia and seccion:
+        raise NotImplementedError
+    else:
+        raise AttributeError('You must specifiy either provincia or seccion or both')
+    return urls
+
 # date = (year, month, date) or datetime.date
 # "http://www.boe.es/diario_borme/xml.php?id=BORME-S-20150601"
 def get_url_xml(date):
+    """ Obtiene el archivo XML que contiene las URLs de los BORMEs del dia indicado """
     if isinstance(date, tuple):
         date = datetime.date(year=date[0], month=date[1], day=date[2])
 
@@ -152,3 +171,22 @@ def download_url(url, filename, timeout=TIMEOUT):
             fd.write(chunk)
 
     return True
+
+
+def download_urls(urls, path):
+    """ Descarga las urls a path indicado """
+    files = []
+    for url in urls.values():
+        filename = url.split('/')[-1]
+        full_path = os.path.join(path, filename)
+        downloaded = download_url(url, full_path)
+
+        if downloaded:
+            files.append(full_path)
+            logger.debug('Downloaded %s' % filename)
+        else:
+            logger.error('Error downloading %s' % url)
+
+        #assert os.path.exists(filepdf)
+        #assert os.path.getsize(filepdf) == int(url.attrib['szBytes'])
+    return files
