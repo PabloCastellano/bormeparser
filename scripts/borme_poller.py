7ffb63912b4008a4026d2b6f255ffecd075fc571
Pablo Castellano <pablo@anche.no>
2015-12-21 16:13:01 +0100

    cabeceras GPL3+

--- scripts/borme_poller.py
+++ scripts/borme_poller.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
-# El BORME se publica los días laborables y normalmente a las 7:30 de la mañana
+# -*- coding: utf-8 -*-
+#
+# borme_poller.py -
+# Copyright (C) 2015 Pablo Castellano <pablo@anche.no>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+# El BORME se publica los días laborables y normalmente a las 7:30 de la mañana
 import datetime
 import time
 
