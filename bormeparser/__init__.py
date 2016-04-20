07e34ec1d8ff806060aed1ed8913cabde5609092
Pablo Castellano <pablo@anche.no>
2016-04-20 15:47:21 +0200

    Emisor

--- bormeparser/__init__.py
+++ bormeparser/__init__.py
@@ -3,6 +3,7 @@ from .borme import Borme, BormeXML
 from .cargo import CARGO
 from .download import download_xml, download_pdfs, download_pdf
 from .download import get_url_pdf, get_url_pdfs, get_url_xml
+from .emisor import EMISOR
 from .parser import parse
 from .provincia import PROVINCIA
 from .seccion import SECCION
---
+++ bormeparser/emisor.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# emisor.py -
+# Copyright (C) 2016 Pablo Castellano <pablo@anche.no>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class EMISOR:
+    # Sección A
+    ACTOS_INSCRITOS = 'Actos inscritos'
+
+    # Sección B
+    OTROS_ACTOS = 'Otros actos publicados en el Registro Mercantil'
+
+    # Sección C
+    BALANCES = 'BALANCES'
+    CONVOCATORIAS_JUNTAS = 'CONVOCATORIAS DE JUNTAS'
+    ESCISIONES = 'ESCISIÓN DE EMPRESAS'
+    TRANSFORMACIONES = 'TRANSFORMACIÓN DE EMPRESAS'
+    FUSIONES_ABORCIONES = 'FUSIONES Y ABSORCIONES DE EMPRESAS'
+    AUMENTO_CAPITAL = 'AUMENTO DE CAPITAL'
+    REDUCCION_CAPITAL = 'REDUCCIÓN DE CAPITAL'
+    OTROS_ANUNCIOS_AVISOS_LEGALES = 'OTROS ANUNCIOS Y AVISOS LEGALES'
