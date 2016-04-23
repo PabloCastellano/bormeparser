0d0693835c0022e695817127cfcd6660d1055267
Pablo Castellano <pablo@anche.no>
2016-04-24 01:02:37 +0200

    borme_to_json.py: Faltaba un parámetro en parse()

--- scripts/borme_to_json.py
+++ scripts/borme_to_json.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 
     # filename
     filename = os.path.basename(sys.argv[1]).replace('.pdf', '.json')
-    borme = bormeparser.parse(sys.argv[1])
+    borme = bormeparser.parse(sys.argv[1], bormeparser.SECCION.A)
     borme.to_json(filename)
 
     print()
